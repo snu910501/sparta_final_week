e95d61c02f099e09cf0c6079c2fcbfc02f8fc90e
TaeHyeongKwon <1004kth1313@naver.com>
2023-01-06 11:18:41 +0900

    feat: Add validation of post N comment

---
+++ validations/comment.validation.js
@@ -0,0 +1,24 @@
+const joi = require('joi');
+
+const createCommentValidation = joi.object().keys({
+  userId: joi.number().required(),
+  postId: joi.number().required(),
+  content: joi.string().required().min(1).max(200),
+});
+
+const updateCommentValidation = joi.object().keys({
+  userId: joi.number().required(),
+  commentId: joi.number().required(),
+  content: joi.string().required().min(1).max(200),
+});
+
+const deleteCommentValidation = joi.object().keys({
+  userId: joi.number().required(),
+  commentId: joi.number().required(),
+});
+
+module.exports = {
+  createCommentValidation,
+  updateCommentValidation,
+  deleteCommentValidation,
+};
