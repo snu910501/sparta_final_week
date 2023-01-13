23b033906ef8fcd790c38af5b4459ab63c01b677
TaeHyeongKwon <1004kth1313@naver.com>
2023-01-13 20:33:35 +0900

    feat/ Add errorHandler

---
+++ modules/errorHandler.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err);
+  if (err.isBoom)
+    res.status(err.output.statusCode).json({ errorMsg: err.message });
+  else if (err.isJoi) res.status(400).json({ errorMsg: '유효성 검사 오류' });
+  else res.status(500).json({ errorMsg: '알 수 없는 오류' });
+};
+
+module.exports = errorHandler;
