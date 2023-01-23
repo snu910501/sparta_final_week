fb20377c315d3902816d9829ee5ad4a25f413de6
Park youngho <snu910501@naver.com>
2023-01-23 11:11:24 +0900

    image file type fix

--- modules/imagesValidate.js
+++ modules/imagesValidate.js
@@ -7,18 +7,21 @@ module.exports = imagesValidate = async (images) => {
     let fileDot = fileName.lastIndexOf('.');
     let fileType = fileName.substring(fileDot + 1, fileName.length).toLowerCase();
 
+    console.log('zz', fileName, fileDot, fileType);
+
     if (pattern.test(fileName)) {
       const error = new Error(405, '파일명에 특수문자가 포함되어 있습니다.')
       throw error
     }
 
     if (fileType == 'png' || fileType == 'jpg' || fileType == 'jpeg') {
-      images[i].originalname = Math.random().toString(30).substring(2, 11);
+      images[i].originalname = `${Math.random().toString(30).substring(2, 11)}.${fileType}`;
 
     } else {
       const error = new Error(405, '확장자명은 png, jpg, jpeg만 가능합니다.')
       throw error
     }
   };
+
   return;
 }
