9bf104afffb605a92198da5c898611061192f603
TaeHyeongKwon <1004kth1313@naver.com>
2023-02-03 11:22:23 +0900

    fix/Add error original message

--- modules/errorHandler.js
+++ modules/errorHandler.js
@@ -2,7 +2,10 @@ const errorHandler = (err, req, res, next) => {
   console.error(err);
   if (err.isBoom)
     res.status(err.output.statusCode).json({ errorMsg: err.message });
-  else if (err.isJoi) res.status(400).json({ errorMsg: '유효성 검사 오류' });
+  else if (err.isJoi)
+    res
+      .status(400)
+      .json({ errorMsg: '유효성 검사 오류', originalMsg: err.message });
   else res.status(500).json({ errorMsg: '알 수 없는 오류' });
 };
 
